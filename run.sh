96c34b026ce94ba7bd7c53584e2c2b991c870b2d
Adam YD <adamyd18@gmail.com>
2024-11-10 12:15:54 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/hexlo/minecraft-bedrock-server.git mc --depth=1
+# Install Playit
+echo "Installing Playit"
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+sudo apt update
+sudo apt install playit
+echo "Done"
+
+mkdir -p mc
 cd mc
+curl https://raw.githubusercontent.com/hexlo/minecraft-bedrock-server/refs/heads/main/.scripts/download-latest-version.sh | bash
+tmux new-session -d -s './start-server.sh'
+playit
