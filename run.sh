7528333769b1b13c061faa80c1de1383c4b2e7bb
Adam YD <adamyd18@gmail.com>
2024-11-10 13:03:16 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,18 @@ sudo apt update
 sudo apt install playit -y
 echo "Done"
 
+# Install Ngrok
+echo "Installing Ngrok"
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
+
+# Start the server
 mkdir -p mc
 cd mc
 curl https://raw.githubusercontent.com/hexlo/minecraft-bedrock-server/refs/heads/main/.scripts/download-latest-version.sh | bash
 timeout 300m tmux new-session -d -s './start-server.sh'
-timeout 300m playit
+timeout 300m ngrok config add-authtoken 1rpIxjnw9FDx2Fcl3xZqWI3SsWr_59uKERPhAS7bn8cMTQX1P
